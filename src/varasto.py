b4cb2e7524e0c4f8f26354334271ff615530c19b
Lahtinen Konsta Aleksi <konsta.a.lahtinen@student.jyu.fi>
2025-10-28 17:46:45 +0200

    modification

--- src/varasto.py
+++ src/varasto.py
@@ -16,7 +16,7 @@ class Varasto:
             # täyteen ja ylimäärä hukkaan!
             self.saldo = tilavuus
 
-    #Testi
+    
     # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
         return self.tilavuus - self.saldo
